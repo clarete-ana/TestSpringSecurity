e03ff573dad3694a2c77acb90473e2993619db7b
Clarete-ana <anaclaraclarete2@gmail.com>
2025-04-23 14:18:30 -0300

    Adicionada anotação @PreAuthorize

--- src/main/java/dio/spring/security/TestController.java
+++ src/main/java/dio/spring/security/TestController.java
@@ -1,17 +1,25 @@
 package dio.spring.security;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
     public class TestController {
 
+        @GetMapping("/")
+        public String home(){
+            return "Bem-vindo!";
+        }
+
         @GetMapping("/users")
+        @PreAuthorize("hasAnyRole('MANAGERS','USERS')")
         public String users() {
             return "Área de usuários";
         }
 
         @GetMapping("/managers")
+        @PreAuthorize("hasRole('MANAGERS')")
         public String managers() {
             return "Área de gerentes";
         }
